2d73b778e1570068f63205ecfaafeeac4555f93a
Arjaan Buijk <arjaan.buijk@gmail.com>
2017-01-27 23:27:17 -0500

    Refactor code.

--- P1.py
+++ P1.py
@@ -90,96 +90,7 @@ def draw_lines(img, lines, color=[255, 0, 0], thickness=2):
         for x1,y1,x2,y2 in line:
             cv2.line(img, (x1, y1), (x2, y2), color, thickness)
 
-def draw_lanes(img, roi_vertices, lines, color=[255, 0, 0], thickness=None):
-    """
-    based on draw_lines
-    """
-    #
-    # extrapolate start and end points for each line to boundary of region of interest
-    #
-    y_top_roi    = roi_vertices[0][1][1]
-    y_bot_roi    = roi_vertices[0][0][1]
-    
-    x_top_roi_min   = roi_vertices[0][1][0]
-    x_top_roi_max   = roi_vertices[0][2][0]
-    x_bot_roi_min   = roi_vertices[0][0][0]
-    x_bot_roi_max   = roi_vertices[0][3][0]
-    
-    x_tops_left   = []
-    x_bots_left   = []
-    
-    x_tops_right   = []
-    x_bots_right   = []
-    
-    for line in lines:
-        for x1,y1,x2,y2 in line:
-        
-            # fit a line (y=Ax+B) through this line
-            # np.polyfit() returns the coefficients [A, B] of the fit
-            fit_line = np.polyfit((x1, x2), (y1, y2), 1)
-            A, B = fit_line
-            
-            # skip lines with very small slope (A)
-            if abs(A) < 0.1:
-                continue
-            
-            # calculate intersection with top & bottom boundary of region of interest
-            x_top = (y_top_roi - B) / A
-            x_bot = (y_bot_roi - B) / A
-            
-            # skip this line if either the top or bottom points are outside region of interest
-            if x_top < x_top_roi_min or x_top > x_top_roi_max:
-                continue
-            if x_bot < x_bot_roi_min or x_bot > x_bot_roi_max:
-                continue
-            
-            if A < 0:
-                x_tops_left.append(x_top)
-                x_bots_left.append(x_bot)
-            else:
-                x_tops_right.append(x_top)
-                x_bots_right.append(x_bot)
-            
-            # uncomment this if you want to check what lines are maintained
-            #thickness = 2
-            #cv2.line(img, (x1, y1), (x2, y2), color, thickness)
-    
-    # if we didn't find a top or bottom, then skip this image
-    if (len(x_tops_left) == 0 or
-        len(x_bots_left) == 0 or
-        len(x_tops_right) == 0 or
-        len(x_bots_right) == 0 ):
-        print ('Not all end points found --> skipping this image !!')
-        return
-    
-    x_top_left_average  = int( np.array(x_tops_left).mean()  )
-    x_top_right_average = int( np.array(x_tops_right).mean() )
-    x_bot_left_average  = int( np.array(x_bots_left).mean()  )
-    x_bot_right_average = int( np.array(x_bots_right).mean() )
-    
-    x_top_left_width  = max(x_tops_left)  - min(x_tops_left)
-    x_top_right_width = max(x_tops_right) - min(x_tops_right)
-    x_bot_left_width  = max(x_bots_left)  - min(x_bots_left)
-    x_bot_right_width = max(x_bots_right) - min(x_bots_right)
-    
-    if thickness == None:
-        thickness = int( 0.5*sum( [x_top_left_width ,
-                                   x_top_right_width,
-                                   x_bot_left_width ,
-                                   x_bot_right_width] ) / 4 )
-    
-    # plot left lane                 
-    cv2.line(img, (x_bot_left_average, y_bot_roi), 
-                  (x_top_left_average, y_top_roi), color, thickness)
-    
-    # plot right lane                 
-    cv2.line(img, (x_bot_right_average, y_bot_roi), 
-                  (x_top_right_average, y_top_roi), color, thickness)
 
-    
-    #for line in lines:
-    #    for x1,y1,x2,y2 in line:
-    #        cv2.line(img, (x1, y1), (x2, y2), color, thickness)
     
 def hough_lines(img, rho, theta, threshold, min_line_len, max_line_gap):
     """
@@ -220,36 +131,87 @@ def draw_lanes_on_image(file_in, file_out, show_all=True, show_final=True):
     
     plt.savefig(file_out)
     print('Processed image saved as: ' + file_out)
-
+   
 def process_image(image, show_all=False, show_final=False):
     # NOTE: The output you return should be a color image (3 channel) for processing video below
     # TODO: put your pipeline here,
     # you should return the final output (image with lines are drawn on lanes)
     global frame
     frame += 1
+    
     print('Frame : ', str(frame),'-', type(image), 'with dimesions:', image.shape)
-    if show_all:
-        plt.title('Original Image')
-        plt.imshow(image)  # if you wanted to show a single color channel image called 'gray', for example, call as plt.imshow(gray, cmap='gray')
-        plt.show()
+    if show_all: show_image(image, 'Original Image')
     
     ####################################################################
     # Make it gray scale
     gray = grayscale(image)
-    if show_all:
-        plt.title('Grayscale Image')
-        plt.imshow(gray, cmap='gray')
-        plt.show()
+    if show_all: show_image(gray, 'Grayscale Image', cmap='gray')
+
+    ####################################################################
+    # Next we'll create a masked image with only region of interest (roi)
+    roi_vertices = calculate_region_of_interest(image, show_all)   
+    masked_gray = region_of_interest(gray, roi_vertices)
+    if show_all: show_image(masked_gray, 'Grayscale Region of Interest', cmap='Greys_r')
     
     ####################################################################
-    # Next we'll create a masked image with only region of interest
-    imshape = image.shape
+    # "Equalize" the dark gray levels in region of interest, so the lighter
+    # lines that represent the lanes are more pronounced, and the canny
+    # edge detection will be more precise.
+    # 
+    # I did this to get challenge_frame_110 to work properly:
+    # (-) The road is concrete (light)
+    # (-) There are black tire skid marks that are detected by canny and
+    #      mess it all up without this step
+    #
+    equalize_gray_level_in_roi(masked_gray)
+    if show_all: show_image(masked_gray, 'Equalized gray level', cmap='Greys_r')
+    
+    ####################################################################
+    edges = find_edges_with_canny(masked_gray)
+    if show_all: show_image(edges, 'Canny Edges', cmap='Greys_r')
+    
+    ####################################################################
+    # Find the lanes, using hough transform
+    #
+    # Define the Hough transform parameters
+    rho             = 2          # distance resolution in pixels of the Hough grid
+    theta           = np.pi/180  # angular resolution in radians of the Hough grid
+    threshold       = 15         # minimum number of votes (intersections in Hough grid cell)
+    min_line_len    = 10         # minimum number of pixels making up a line
+    max_line_gap    = 5          # maximum gap in pixels between connectable line segments
+    
+    lines = cv2.HoughLinesP(edges, rho, theta, threshold, np.array([]), minLineLength=min_line_len, maxLineGap=max_line_gap)
+    if lines is None :
+        print ('ERROR: No hough lines found')
+        return image
+    
+    if show_all:
+        line_img = np.zeros((edges.shape[0], edges.shape[1], 3), 
+                            dtype=np.uint8)
+        draw_lines(line_img, lines)
+        combo = weighted_img(line_img, image, α=0.8, β=1., λ=0.)
+        show_image(combo, 'Hough Lines on Image')
 
-    # base roi on ratio of image
-    x_bot_min_ratio = 0.156
-    x_bot_max_ratio = 0.950
-    x_top_min_ratio = x_bot_min_ratio
-    x_top_max_ratio = x_bot_max_ratio
+    ####################################################################
+    # Draw the lanes on the original image    
+    line_img = np.zeros((image.shape[0], image.shape[1], 3), 
+                        dtype=np.uint8)
+    create_lanes_from_hough_lines(line_img, roi_vertices, lines, thickness=10)
+    combo = weighted_img(line_img, image, α=0.8, β=1., λ=0.)
+    if show_all or show_final: show_image(combo, 'Final Image with Lanes')
+
+    return combo
+
+def show_image(image, title, cmap=None ):
+    plt.title(title)
+    if cmap:
+        plt.imshow(image, cmap=cmap) # if you wanted to show a single color channel image called 'gray', for example, call as plt.imshow(gray, cmap='gray')
+    else:
+        plt.imshow(image)  
+    plt.show()
+    
+def calculate_region_of_interest(image, show_all):
+    imshape = image.shape
     
     # height of roi region
     if roi_height == 0:
@@ -268,8 +230,7 @@ def process_image(image, show_all=False, show_final=False):
         width_top = int( 0.20 * imshape[1] )
     else:
         width_top = roi_width_top
-    
-    
+      
     x_bot_min = roi_shift_bot + int( 0.5 * (imshape[1] - width_bot) )
     x_bot_max = roi_shift_bot + int( 0.5 * (imshape[1] + width_bot) )
     x_top_min = roi_shift_top + int( 0.5 * (imshape[1] - width_top) )
@@ -295,110 +256,137 @@ def process_image(image, show_all=False, show_final=False):
         line_img = np.zeros((image.shape[0], image.shape[1], 3), 
                             dtype=np.uint8)
         p=roi_vertices[0]
-        roi_lines = np.array([[[ p[0][0], p[0][1], p[1][0], p[1][1] ]],
-                              [[ p[1][0], p[1][1], p[2][0], p[2][1] ]],
-                              [[ p[2][0], p[2][1], p[3][0], p[3][1] ]],
-                              [[ p[3][0], p[3][1], p[0][0], p[0][1] ]]], 
+        roi_lines = np.array([[[ p[0, 0], p[0, 1], p[1, 0], p[1, 1] ]],
+                              [[ p[1, 0], p[1, 1], p[2, 0], p[2, 1] ]],
+                              [[ p[2, 0], p[2, 1], p[3, 0], p[3, 1] ]],
+                              [[ p[3, 0], p[3, 1], p[0, 0], p[0, 1] ]]], 
                              dtype=np.int32)
         draw_lines(line_img, roi_lines)
         combo = weighted_img(line_img, image, α=0.8, β=1., λ=0.)
         plt.imshow(combo)
         plt.show()
-    
-    masked_gray = region_of_interest(gray, roi_vertices)
+        
+    return roi_vertices
 
-    # Display the image
-    if show_all:
-        plt.title('Grayscale with Region of Interest')
-        plt.imshow(masked_gray, cmap='Greys_r')
-        plt.show()
-    
-    ####################################################################
-    # Set gray level in region of interest to black
-    #  if it is darker than the average in the roi.
-    # 
-    # I did this to get challenge_frame_110 to work properly:
-    # (-) The road is concrete (light)
-    # (-) There are black tire skid marks that are detected by canny and
-    #      mess it all up..
-    #
-    # find the average gray level in roi
-    
+def equalize_gray_level_in_roi(masked_gray): 
+    # calculate the average gray level in the region of interest   
     gray_average = int( np.average(masked_gray[np.nonzero(masked_gray)]) )
     
-#     for i in range(masked_gray.shape[0]):
-#         for j in range(masked_gray.shape[1]):
-#             if masked_gray[i][j] < gray_average:
-#                 masked_gray[i][j] = gray_average 
-    # fast solution: http://stackoverflow.com/questions/19666626/replace-all-elements-of-python-numpy-array-that-are-greater-than-some-value
+    # assign this average gray level to all regions that are darker than this
+    # average value
     masked_gray[ masked_gray < gray_average ] = gray_average
-    
-    # Display the image
-    if show_all:
-        plt.title('Equalized gray level')
-        plt.imshow(masked_gray, cmap='Greys_r')
-        plt.show()
-    
-    ####################################################################
-    # Find the edges, with canny edge detection
+
+def find_edges_with_canny(masked_gray):    
     # See: http://homepages.inf.ed.ac.uk/rbf/HIPR2/canny.htm
     #
     # Define a kernel size for Gaussian smoothing / blurring
     # Note: this step is optional as cv2.Canny() applies a 5x5 Gaussian internally
     kernel_size = 5
-    gaussian_blur(image, kernel_size)
+    gaussian_blur(masked_gray, kernel_size)
     
     # Define parameters for Canny and run it
     # thresholds between 0 and 255
     low_threshold = 50
     high_threshold = 150
-    #low_threshold = 50
-    #high_threshold = 75
     edges = canny(masked_gray, low_threshold, high_threshold)
     
-    # Display the image
-    if show_all:
-        plt.title('Canny Edges')
-        plt.imshow(edges, cmap='Greys_r')
-        plt.show()
+    return edges 
+   
+def create_lanes_from_hough_lines(img, roi_vertices, lines, color=[255, 0, 0], thickness=None):
+    """
+    This routine attempts to extract the left and right lane from the provided
+    hough lines, and draws them on the image.
     
-    
-    
-    ####################################################################
-    # Find the lanes, using hough transform
+    The method used is as follows:
+    (-) each line is extrapolated to the top and bottom of the region of interest
+    (-) it will be discarded if the intersection at the top and bottom is outside
+        the region of interest.
+    (-) the remaining lines are assigned to the left lane if the angle is negative,
+        else they are assigned to the right lane
+    (-) the locations of the line intersections with the top and bottom of 
+        the region of interest are averaged to provide the lane end points
+        
+    NOTE: This logic ONLY works if the lanes within the region of interest are
+          straight.
+    """
     #
-    # Define the Hough transform parameters
-    rho             = 2          # distance resolution in pixels of the Hough grid
-    theta           = np.pi/180  # angular resolution in radians of the Hough grid
-    threshold       = 15         # minimum number of votes (intersections in Hough grid cell)
-    min_line_len    = 10         # minimum number of pixels making up a line
-    max_line_gap    = 5          # maximum gap in pixels between connectable line segments
+    # extrapolate start and end points for each line to boundary of region of interest
+    #
+    y_top_roi    = roi_vertices[0, 1, 1]
+    y_bot_roi    = roi_vertices[0, 0, 1]
     
-    lines = cv2.HoughLinesP(edges, rho, theta, threshold, np.array([]), minLineLength=min_line_len, maxLineGap=max_line_gap)
-    if lines is None :
-        print ('ERROR: No hough lines found')
-        return image
+    x_top_roi_min   = roi_vertices[0, 1, 0]
+    x_top_roi_max   = roi_vertices[0, 2, 0]
+    x_bot_roi_min   = roi_vertices[0, 0, 0]
+    x_bot_roi_max   = roi_vertices[0, 3, 0]
     
-    if show_all:
-        plt.title('Hough Lines on Image')
-        line_img = np.zeros((edges.shape[0], edges.shape[1], 3), 
-                            dtype=np.uint8)
-        draw_lines(line_img, lines)
-        combo = weighted_img(line_img, image, α=0.8, β=1., λ=0.)
-        plt.imshow(combo)
-        plt.show()
-
-    # Draw the lanes on the original image    
-    line_img = np.zeros((edges.shape[0], edges.shape[1], 3), 
-                        dtype=np.uint8)
-    draw_lanes(line_img, roi_vertices, lines, thickness=10)
-    combo = weighted_img(line_img, image, α=0.8, β=1., λ=0.)
-    if show_all:
-        plt.title('Final Image with Lanes')
-        plt.imshow(combo)
-        plt.show()
-
-    return combo
+    x_tops_left   = []
+    x_bots_left   = []
+    
+    x_tops_right   = []
+    x_bots_right   = []
+    
+    for line in lines:
+        for x1,y1,x2,y2 in line:
+        
+            # fit a line (y=Ax+B) through this line
+            # np.polyfit() returns the coefficients [A, B] of the fit
+            fit_line = np.polyfit((x1, x2), (y1, y2), 1)
+            A, B = fit_line
+            
+            # skip lines with very small slope (A)
+            if abs(A) < 0.1:
+                continue
+            
+            # calculate intersection with top & bottom boundary of region of interest
+            x_top = (y_top_roi - B) / A
+            x_bot = (y_bot_roi - B) / A
+            
+            # skip this line if either the top or bottom points are outside region of interest
+            if x_top < x_top_roi_min or x_top > x_top_roi_max:
+                continue
+            if x_bot < x_bot_roi_min or x_bot > x_bot_roi_max:
+                continue
+            
+            # negative angle line --> left lane
+            if A < 0:
+                x_tops_left.append(x_top)
+                x_bots_left.append(x_bot)
+            else:
+                x_tops_right.append(x_top)
+                x_bots_right.append(x_bot)
+    
+    # if we didn't find a top or bottom, then skip this image
+    if (len(x_tops_left) == 0 or
+        len(x_bots_left) == 0 or
+        len(x_tops_right) == 0 or
+        len(x_bots_right) == 0 ):
+        print ('Not all end points found --> skipping this image !!')
+        return
+    
+    x_top_left_average  = int( np.array(x_tops_left).mean()  )
+    x_top_right_average = int( np.array(x_tops_right).mean() )
+    x_bot_left_average  = int( np.array(x_bots_left).mean()  )
+    x_bot_right_average = int( np.array(x_bots_right).mean() )
+    
+    x_top_left_width  = max(x_tops_left)  - min(x_tops_left)
+    x_top_right_width = max(x_tops_right) - min(x_tops_right)
+    x_bot_left_width  = max(x_bots_left)  - min(x_bots_left)
+    x_bot_right_width = max(x_bots_right) - min(x_bots_right)
+    
+    if thickness == None:
+        thickness = int( 0.5*sum( [x_top_left_width ,
+                                   x_top_right_width,
+                                   x_bot_left_width ,
+                                   x_bot_right_width] ) / 4 )
+    
+    # plot left lane                 
+    cv2.line(img, (x_bot_left_average, y_bot_roi), 
+                  (x_top_left_average, y_top_roi), color, thickness)
+    
+    # plot right lane                 
+    cv2.line(img, (x_bot_right_average, y_bot_roi), 
+                  (x_top_right_average, y_top_roi), color, thickness)
 
 if __name__ == '__main__':
     # Import everything needed to edit/save/watch video clips
@@ -427,7 +415,7 @@ if __name__ == '__main__':
         frame = 0
         draw_lanes_on_image(file_in=file_dir+"/"+file, 
                             file_out=file_dir+"/with_lanes_"+file,
-                            show_all=False, show_final=False)
+                            show_all=False, show_final=True)
           
     ################################################################
     # process some test videos
